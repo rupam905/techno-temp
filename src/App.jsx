c6792a5205bfe93cd9eb963d582c1fc800d5bcfa
rupam905 <rupamdutta905.com>
2025-07-12 13:46:51 +0530

    some adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,6 @@ import Home from "./pages/Home";
 const App = () => {
   return (
     <>
-      {/* <h1 className="px-4 py-2 bg-indigo-600 hover:bg-indigo-700 text-white rounded-md transition-colors">
-        Let's strat building guys
-      </h1> */}
       <div className="min-h-screen">
         <Head />
         <Home />
