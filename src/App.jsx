ffcef84d4c32c25a322e115972d3136c46078f75
Rima Paul <paulrima2730@gmail.com>
2025-07-13 22:50:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Contact from "./pages/Contact";
+import Events from "./pages/Events";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/events" element={<Events />} />
           </Routes>
           <Footer />
         </div>
